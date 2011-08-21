3c454ea86a477e8b9266c544f5dfd39936c55acb
Sasu Karttunen <sasu.karttunen@tpnet.fi>
2011-08-21 17:23:59 +0300

    added README and COPYING

--- contents/code/main.py
+++ contents/code/main.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#    Copyright 2011 Sasu Karttunen <sasu.karttunen@tpnet.fi>
+#
+#    This file is part of PlasmaFreq.
+#
+#    PlasmaFreq is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PlasmaFreq is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PlasmaFreq.  If not, see <http://www.gnu.org/licenses/>.
+
 from PyQt4.QtCore import Qt, QFileSystemWatcher, QObject, SIGNAL
 from PyQt4.QtGui import QGraphicsLinearLayout, QButtonGroup, QSizePolicy
 from PyKDE4.plasma import Plasma
